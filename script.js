6fc047372f4c3533cd834110ea99807454b4abce
Kari <kouglas2@gmail.com>
2022-11-02 20:30:14 -0500

    refactor commit
    remove some parts of the getColorScheme function as they were ultimately not needed for the program to work. remove some unused styles / selectors. still need to make this program responsive to mobile / resizing

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// https://www.thecolorapi.com/docs#schemes check here the words in red point to why some
+// of these consts were included in this function
 // taking control of elements by id based on URI parameters set by the color api
 const mode = document.getElementById('mode') // list color schemes aka color mode defines mode by which to generate the scheme from seed color
 const seedColor = document.getElementById('seedColor') // starting color aka seed color
@@ -8,24 +10,30 @@ const count = 5 // number of colors to return
 // button that takes a function called getColorScheme
 document.querySelector('button').addEventListener('click', getColorScheme)
 
-
 // the function will fetch the colors api
 // and it will display the the color scheme
 // in the 'color-palette div' 
 function getColorScheme() {
+    //seedColor.value = an html id of seedColor and then value is supplied by the color picker
+    // mode.value = an html id of mode and then value is supplied by the values that are inside of the option tags
     fetch(`https://www.thecolorapi.com/scheme?hex=${seedColor.value.slice(1)}&mode=${mode.value}&count=${count}`)
         .then((res) => res.json())
         .then((newColor) => {
             let html = "" // need these both set to empty strings so they can be populated below
             let codes = ""
             for(let i = 0; i < 5; i++) {
+                // this is the html we want to return in the colorPalette div 
+                // it adds / returns 5 colors based of the selected scheme
+                // iterating through 5 values with the for loop
+                // the css dictates how the 5 colors are presented (big main section)
+                // here we get hex values and display them as actual colors
+                // the id and class inside of the template literal are selectors for styling purposes 
                 html += `
-                    <div id="colorColumn" class="colorPalette column" style="background-color: ${newColor.colors[i].hex.value}" title=${newColor.colors[i].hex.value}>
-                        <p class="columnCode" style="visibility: hidden">${newColor.colors[i].hex.value}
-                        </p>
+                    <div id="colorColumn" class="colorPalette column" style="background-color: ${newColor.colors[i].hex.value}">
                     </div>
                     `
-            codes += `
+                // here we get hex values and display them as actual values inside of a p tag
+                codes += `
                 <div><p>${newColor.colors[i].hex.value}</p></div>
             `            
         }
@@ -34,11 +42,8 @@ function getColorScheme() {
 
         //inject hex values for html to display
         hexValues.innerHTML = codes 
-
     })
-
 }
-
 getColorScheme()
 
 
